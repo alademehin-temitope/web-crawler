c48f64edd2d31262386ff20532c7a05014112582
alademehin-temitope <alademehintemitoper@gmail.com>
2016-09-04 00:02:00 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,27 @@
+
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'https://ndb.nal.usda.gov/ndb/search'
+
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+table = soup.find('table', attrs={'width': '100%'})
+
+list_of_rows = []
+for row in table.findAll('tr'):
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        text = cell.text.replace('&#39;', '')
+
+        list_of_cells.append(text)
+
+	list_of_rows.append(list_of_cells)
+
+outfile = open("./inmate.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerow(["Number", "food", "Nutritions"])
+writer.writerows(list_of_rows)
